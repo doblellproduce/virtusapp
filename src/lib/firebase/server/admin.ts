81c72629f8a799f2d6036ef271a0831fc3a4f8b8
Luis Manon <soypromord@gmail.com>
2025-07-24 18:54:48 +0000

    verifica esto y .. y tambien te compartire algunos datos de mi congigura

--- src/lib/firebase/server/admin.ts
+++ src/lib/firebase/server/admin.ts
@@ -7,6 +7,7 @@ import { getStorage, Storage } from 'firebase-admin/storage';
 const serviceAccount = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  // Correctly format the private key by replacing \\n with \n
   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
 
