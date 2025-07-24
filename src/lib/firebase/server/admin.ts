03fdfea899a913d5da5a660ef186c01c5853bc57
Luis Manon <soypromord@gmail.com>
2025-07-24 19:56:22 +0000

    stop//

--- src/lib/firebase/server/admin.ts
+++ src/lib/firebase/server/admin.ts
@@ -14,14 +14,16 @@ const privateKey = (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'
 const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
 const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
 
+const serviceAccount = {
+  projectId: projectId,
+  clientEmail: clientEmail,
+  privateKey: privateKey,
+};
+
 if (getApps().length === 0) {
   if (privateKey && clientEmail && projectId) {
     adminApp = initializeApp({
-      credential: cert({
-        projectId,
-        clientEmail,
-        privateKey,
-      }),
+      credential: cert(serviceAccount),
       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     });
   } else {
