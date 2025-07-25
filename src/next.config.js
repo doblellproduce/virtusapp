1f524f40407f83ae87ed56183046f1fa7447e119
Luis Manon <soypromord@gmail.com>
2025-07-25 03:56:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/next.config.js
+++ src/next.config.js
@@ -13,38 +13,7 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    // This webpack configuration is only applied to the client-side bundle.
-    if (!isServer) {
-      // Provide fallbacks for Node.js built-in modules that are used by some of the libraries.
-      // This tells Webpack to use browser-compatible versions of these modules.
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        "process": require.resolve("process/browser"),
-        "zlib": require.resolve("browserify-zlib"),
-        "stream": require.resolve("stream-browserify"),
-        "util": require.resolve("util/"),
-        "assert": require.resolve("assert/"),
-      };
-      
-      // The ProvidePlugin makes a module available as a variable in every module.
-      // This is necessary for some libraries that expect 'process' to be global.
-      const webpack = require('webpack');
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          process: "process/browser",
-        })
-      );
-    }
-
-    // Exclude firebase-admin from client-side bundles.
-    // This is crucial because firebase-admin is a server-only SDK.
-    if (!isServer) {
-      config.externals.push('firebase-admin');
-    }
-
-    return config;
-  },
+  instrumentationHook: false,
 };
 
 module.exports = nextConfig;
