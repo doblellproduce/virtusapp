7f982577e77f0924a344aab09a765903f3e97151
Luis Manon <soypromord@gmail.com>
2025-07-25 19:07:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Excluir firebase-admin del paquete del cliente,
+    // ya que solo debe ejecutarse en el servidor.
+    if (!isServer) {
+      config.externals.push('firebase-admin');
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
