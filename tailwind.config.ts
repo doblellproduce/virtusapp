5f3455520e91553ea3e265cc8932e085b5072c22
Luis Manon <soypromord@gmail.com>
2025-07-26 01:54:39 +0000

    🔧 Realiza una auditoría y corrección automática del sistema de estilos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,7 @@ import type {Config} from 'tailwindcss';
 export default {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{ts,tsx,js,jsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
